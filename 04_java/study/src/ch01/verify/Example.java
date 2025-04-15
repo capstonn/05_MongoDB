e6462d73180cbacc9b45b56b5b9424fc2a5d9c1b
capstonn <suhyun6655@naver.com>
2025-04-15 16:40:18 +0900

    20240414 과제

---
+++ 04_java/study/src/ch01/verify/Example.java
@@ -0,0 +1,9 @@
+package ch01.verify;
+
+public class Example {
+    public static void main(String[] args) {
+        System.out.println("개발자가 되기 위한 필수 개발 언어 Java");
+    }
+}
+// 다음 코드의 문제점을 찾아 설명해보세요
+// -> 초기화되지 않은 변수를
---
+++ 04_java/study/src/ch02/sec13/Width.java
@@ -0,0 +1,13 @@
+package ch02.sec13;
+
+public class Width {
+    public static void main(String[] args) {
+        double top = 5;
+        double bottom = 10;
+        double height = 7;
+
+        double area = (top + bottom) * height / 2.0;
+
+        System.out.println("사다리꼴의 넓이: " + area);
+    }
+}
